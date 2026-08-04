846158d1aa0542ee134fe968be8aeb3bc023fdd9
jihoo414-tech <jihoo414@gmail.com>
2026-08-04 12:44:27 +0900

    init

---
+++ src/test/java/com/example/p_63_260804/P63260804ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p_63_260804;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P63260804ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
