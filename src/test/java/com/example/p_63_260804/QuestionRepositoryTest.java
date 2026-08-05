cf895f38aab9a3f7c9dc1e4982ef45c094bde7e7
jihoo414-tech <jihoo414@gmail.com>
2026-08-05 11:09:40 +0900

    add transactional annotation to modify test

--- src/test/java/com/example/p_63_260804/QuestionRepositoryTest.java
+++ src/test/java/com/example/p_63_260804/QuestionRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -65,6 +66,7 @@ class QuestionRepositoryTest {
 
 	@Test
 	@DisplayName("데이터 수정")
+	@Transactional
 	void t6() {
 		Optional<Question> oq = this.questionRepository.findById(1);
 		assertTrue(oq.isPresent());
