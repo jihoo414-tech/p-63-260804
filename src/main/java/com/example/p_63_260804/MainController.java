1a8a027a3d08f9b1519c0d01248924117c4e056e
jihoo414-tech <jihoo414@gmail.com>
2026-08-05 09:31:00 +0900

    4,5강

--- src/main/java/com/example/p_63_260804/MainController.java
+++ src/main/java/com/example/p_63_260804/MainController.java
@@ -2,20 +2,27 @@ package com.example.p_63_260804;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 //Spring컨테이너에 빈으로 등록 + web환경에서 접근할 수 있도록 추가구현을 해줌
 @Controller //브라우저를 통해 해당 클래스 함수를 호출할 수 있다.
+
 public class MainController {
 
     @GetMapping("/")
-    public void index() {
+    @ResponseBody
+    public String index() {
         System.out.println("index() 메서드 호출됨!");
+        return "index() 메서드 호출됨";
     }
 
     @GetMapping("/hello")
-    public void hello() {
-        System.out.println("hello() 메서드 호출됨!");
+    @ResponseBody //액션 메서드의 반환값을 HTTP응답 본문으로 사용하도록 지정하는 애너테이션
+    public String hello() {
+        System.out.println("hello() 메서드 호출됨!"); // 서버콘솔에 출력
+        return "hello() 메서드 호출됨"; //고객(요청한 쪽)의 브라우저에 출력  return
     }
+
 
 
 }
--- src/test/java/com/example/p_63_260804/P63260804ApplicationTests.java
+++ src/test/java/com/example/p_63_260804/P63260804ApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.p_63_260804;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class P63260804ApplicationTests {
 
 	@Test
