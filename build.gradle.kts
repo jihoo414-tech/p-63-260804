c570d1a74924d7887c38ac15cd253092fe119c99
jihoo414-tech <jihoo414@gmail.com>
2026-08-05 10:24:50 +0900

    8,9강

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	testCompileOnly("org.projectlombok:lombok")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testAnnotationProcessor("org.projectlombok:lombok")
+	implementation("org.springframework.boot:spring-boot-h2console")
 	runtimeOnly ("com.h2database:h2")
 	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
 }
--- src/main/java/com/example/p_63_260804/Question.java
+++ src/main/java/com/example/p_63_260804/Question.java
@@ -4,6 +4,8 @@ package com.example.p_63_260804;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Question {
@@ -19,4 +21,10 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate;
+
+
+    // cascade = CascadeType.REMOVE를 적으면 question을 remove하면 answer도 자동으로 remove됨
+    @OneToMany(mappedBy = "question",cascade = CascadeType.REMOVE)// mappedBy가 붙은 쪽은 외래키를 만들지 않고 반대쪽에 만든다.
+    private List<Answer> answers = new ArrayList<>();
+
 }
